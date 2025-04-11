a5fc126569fa0772b7eb7941f978c2d9aa05c34c
Zachary Stence <zachstence@gmail.com>
2025-04-11 18:12:02 -0500

    vercel adapter options

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,12 @@ const config = {
     // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
-    adapter: adapter(),
+    adapter: adapter({
+      runtime: "nodejs20.x",
+      // Split the edge functions to avoid size limit issues
+      split: true,
+      maxDuration: 300,
+    }),
   },
 };
 
